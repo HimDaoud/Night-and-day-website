edde63e42f7fa13ded5dd6c008786f6b69b7855d
Him Daoud <56063700+HimDaoud@users.noreply.github.com>
2023-05-20 01:25:58 +0200

    Added by Him

---
+++ main.js
@@ -0,0 +1,36 @@
+let stars = document.getElementById('stars');
+let moon = document.getElementById('moon');
+let mountains3 = document.getElementById('mountains3');
+let mountains4 = document.getElementById('mountains4');
+let river = document.getElementById('river');
+let boat= document.getElementById('boat');
+let Him = document.querySelector('.Him');
+
+window.onscroll = function (){
+    let value = scrollY;
+    stars.style.left = value + 'px';
+    moon.style.top = value * 4 + 'px';
+    mountains3.style.top = value * 2 +'px';
+    mountains4.style.top = value * 1.5 +'px';
+    river.style.top = value + 'px';
+    boat.style.top = value + 'px';
+    boat.style.left = value * 3 + 'px';
+    Him.style.fontSize = value + 'px';
+    if(scrollY >=67){
+        Him.style.fontSize = 67 + 'px';
+        Him.style.position = 'fixed';
+        if(scrollY>=478){
+            Him.style.display = 'none';
+        } else{
+            Him.style.display = 'block';
+        }
+        if(scrollY>=127){
+            document.querySelector('.main').style.background ='linear-gradient(#376281,#10001f)'
+        } else{
+            document.querySelector('.main').style.background = 'linear-gradient(#200016,#10001f)'
+        }
+
+    }
+}
+
+
